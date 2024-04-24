30f652a264bf5beb0f532fa09a614612e1bff124
Pinchuk Andrii <pinchuk.andryusha310212@gmail.com>
2024-04-24 20:05:01 +0300

    init repo

---
+++ src/test/java/pawlin/userapi/UserApiApplicationTests.java
@@ -0,0 +1,17 @@
+package pawlin.userapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+class UserApiApplicationTests {
+
+	@Test
+	void contextLoads(ApplicationContext context) {
+		assertNotNull(context);
+	}
+
+}
